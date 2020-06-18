2a0fc2daf08445a30d61cfa2512d1f0be3d8efc5
Brock <brocklyons@MacBook-Pro.local>
2020-06-17 20:31:45 -0700

    Initial XCode project creation

---
+++ youtube-ios-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtube-ios-app
+//
+//  Created by Brock on 6/17/20.
+//  Copyright © 2020 Brock Lyons. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
