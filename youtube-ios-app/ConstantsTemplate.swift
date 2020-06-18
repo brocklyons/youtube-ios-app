6f9600a55337dda7bec3f72d1cf1d61ddfe7a81d
Brock <brocklyons@MacBook-Pro.local>
2020-06-17 22:20:33 -0700

    Added ConstantsTemplate.swift

---
+++ youtube-ios-app/ConstantsTemplate.swift
@@ -0,0 +1,19 @@
+//
+//  ConstantsTemplate.swift
+//  youtube-ios-app
+//
+//  Created by Brock on 6/17/20.
+//  Copyright © 2020 Brock Lyons. All rights reserved.
+//
+
+import Foundation
+
+// Add your own keys below and uncomment the code
+
+/*
+struct Constants {
+    static var API_KEY = ""
+    static var PLAYLIST_ID = ""
+    static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+}
+*/
