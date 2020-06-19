cba0cc0bead5bc59a652c283f5c4dd6d0f2114dd
Brock <brocklyons@MacBook-Pro.local>
2020-06-18 21:04:33 -0700

    Added groups to better organize project

--- youtube-ios-app/Helpers/Constants.swift
+++ youtube-ios-app/Helpers/Constants.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 struct Constants {
-    static var API_KEY = ""
+    static var API_KEY = "test"
     static var PLAYLIST_ID = "UUE_M8A5yxnLfW0KghEeajjw"
     static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
     static var VIDEOCELL_ID = "VideoCell"
